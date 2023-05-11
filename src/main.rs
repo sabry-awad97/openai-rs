19908476053f7c18ce0c848160dc9b492e9bab1d
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-11 16:30:38 +0300

    🐛 fix(main.rs): Update main to use openai-rs ChatClient ✨ feat(main.rs): Use ChatClient to send message to OpenAI API To improve the structure of the code and to better utilize the openai-rs crate, the main function was updated to use the ChatClient. The ChatCompletionRequest and ChatCompletionMessage structs were added to configure the request to the OpenAI API. The response from the API is printed to the console.
    Changes made:
    - Replaced `openai_rs::run()` with a new `ChatClient` instance to communicate with the OpenAI API.
    - Added `ChatCompletionRequest` and `ChatCompletionMessage` structs to configure the request to the OpenAI API.
    - Added `println!()` statement to print the response from the API to the console.
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use dotenv::dotenv;
 use envconfig::Envconfig;
 
-use openai_rs;
+use openai_rs::{
+    self, ChatClient, ChatCompletionMessage, ChatCompletionRequest, MessageRole, OAIModel,
+};
 
 #[derive(Envconfig)]
 pub struct Config {
@@ -16,13 +18,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     env_logger::init();
     let config = Config::init_from_env()?;
-    match openai_rs::run(&config.api_host, &config.openai_api_key).await {
-        Ok(message) => {
-            println!("{}", message);
-        }
-        Err(err) => {
-            eprintln!("{}", err);
-        }
-    }
+    let client = ChatClient::new(&config.api_host, &config.openai_api_key);
+
+    let request = ChatCompletionRequest {
+        model: OAIModel::GPT3Turbo,
+        messages: vec![ChatCompletionMessage {
+            role: MessageRole::User,
+            content: "Hello, GPT-3!".to_string(),
+        }],
+        temperature: Some(0.5),
+        top_p: Some(1.0),
+        max_tokens: Some(50),
+        frequency_penalty: Some(0.0),
+        presence_penalty: Some(0.0),
+    };
+
+    let response = client.run(request).await?;
+    println!("{}", response);
+
     Ok(())
 }
