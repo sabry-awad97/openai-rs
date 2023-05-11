9e2dcef58b7874f27afd11d315f50da01925364a
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-11 19:47:31 +0300

    🐛 fix(openai.rs): Add retries for network errors In the `OpenAIClient` implementation, a loop with a retry mechanism was added to handle network errors that can occur when making an API request. If an error occurs, the loop will retry the request up to three times before returning a `NetworkError` error. Changes made: - A `loop` was added to handle retry attempts. - The `send_request` function was wrapped in a `match` expression to handle both `Ok` and `Err` cases. - A `retries` counter was added to limit the number of retries. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/openai.rs
+++ src/openai.rs
@@ -21,39 +21,54 @@ impl OpenAIClient {
 
     pub async fn send_request(&self, request: ChatCompletionRequest) -> Result<String, ChatError> {
         let url = format!("{}/v1/chat/completions", self.api_host);
-
+    
         debug!("Sending request to URL: {}", url);
         debug!("Request body: {:?}", request);
-
-        let response = self
-            .client
-            .post(url)
-            .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
-            .bearer_auth(&self.api_key)
-            .json(&request)
-            .send()
-            .await?;
-
-        let status = response.status();
-        let response = response.json::<ChatCompletionResponse>().await?;
-
-        debug!("Response status: {}", status);
-
-        if status.is_success() {
-            log::info!("Response: {:?}", response);
-        } else {
-            log::error!("Error: {} - {:?}", status, response);
-            return Err(ChatError::ResponseError(status, response));
+    
+        let mut retries = 0;
+        loop {
+            let response = self
+                .client
+                .post(url.clone())
+                .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
+                .bearer_auth(&self.api_key)
+                .json(&request)
+                .send()
+                .await;
+    
+            match response {
+                Ok(response) => {
+                    let status = response.status();
+                    let response = response.json::<ChatCompletionResponse>().await?;
+    
+                    debug!("Response status: {}", status);
+    
+                    if status.is_success() {
+                        log::info!("Response: {:?}", response);
+    
+                        let result = response
+                            .choices
+                            .first()
+                            .map(|choice| choice.message.content.clone())
+                            .ok_or_else(|| ChatError::NoMessageReturned)?;
+    
+                        debug!("Result: {}", result);
+                        return Ok(result);
+                    } else {
+                        log::error!("Error: {} - {:?}", status, response);
+                        return Err(ChatError::ResponseError(status, response));
+                    }
+                }
+                Err(err) => {
+                    retries += 1;
+                    if retries > 3 {
+                        return Err(ChatError::NetworkError(err));
+                    }
+                    debug!("Error: {}", err);
+                    debug!("Retrying request (attempt {})", retries);
+                }
+            }
         }
-
-        let result = response
-            .choices
-            .first()
-            .map(|choice| choice.message.content.clone())
-            .ok_or_else(|| ChatError::NoMessageReturned)?;
-
-        debug!("Result: {}", result);
-        Ok(result)
     }
 }
 
