8d1fc0487a0cb3cefe1760a976909048caf68e2b
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-11 15:39:07 +0300

    🚀 feat: add OpenAI API chat completion client
    This commit adds a new client for the OpenAI API that can be used to generate chat completions using the GPT-3.5 Turbo model. The client takes a user message as input and sends a request to the API, which responds with a completion for the conversation.
    
    The client uses the `dotenv` and `envconfig` crates to load configuration variables from environment variables. It also uses the `reqwest` crate to send HTTP requests to the API and the `serde` crate to serialize and deserialize JSON.
    
    The `ChatCompletionRequest` struct contains the parameters for the chat completion request, including the model, messages, temperature, top-p, max tokens, frequency penalty, and presence penalty.
    
    The `OAIModel` enum specifies the available OpenAI API models, including the GPT-3.5 Turbo and GPT-4 models.
    
    The `ChatCompletionMessage` struct defines the message format, including the message role and content.
    
    The `ChatCompletionResponse` struct defines the response format, including the completion choices and usage statistics.
    
    Finally, the `main` function initializes the configuration from environment variables, constructs a chat completion request, sends a request to the OpenAI API, and prints the response.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,111 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use envconfig::Envconfig;
+use reqwest::header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE};
+use serde::{Deserialize, Serialize};
+#[derive(Serialize)]
+struct ChatCompletionRequest {
+    model: OAIModel,
+    messages: Vec<ChatCompletionMessage>,
+    temperature: Option<f32>,
+    top_p: Option<f32>,
+    max_tokens: Option<u64>,
+    frequency_penalty: Option<f32>,
+    presence_penalty: Option<f32>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ChatCompletionMessage {
+    pub role: MessageRole,
+    pub content: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum MessageRole {
+    System,
+    User,
+    Assistant,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum OAIModel {
+    #[serde(rename = "gpt-3.5-turbo")]
+    GPT3Turbo,
+    #[serde(rename = "gpt-4")]
+    GPT4,
+}
+
+#[derive(Deserialize)]
+pub struct ChatCompletionResponse {
+    pub id: String,
+    pub object: String,
+    pub created: u64,
+    pub model: String,
+    pub choices: Vec<ChatCompletionChoice>,
+    pub usage: Option<Usage>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Usage {
+    pub prompt_tokens: i64,
+    pub completion_tokens: i64,
+    pub total_tokens: i64,
+}
+
+#[derive(Deserialize)]
+pub struct ChatCompletionChoice {
+    pub index: u64,
+    pub message: ChatCompletionMessage,
+    pub finish_reason: Option<String>,
+}
+
+#[derive(Envconfig)]
+pub struct Config {
+    #[envconfig(from = "OPENAI_API_KEY")]
+    pub openai_api_key: String,
+    #[envconfig(from = "API_HOST", default = "https://api.openai.com")]
+    pub api_host: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let config = Config::init_from_env()?;
+
+    let request = ChatCompletionRequest {
+        model: OAIModel::GPT3Turbo,
+        messages: vec![ChatCompletionMessage {
+            role: MessageRole::User,
+            content: "Hello, GPT-3!".to_string(),
+        }],
+        temperature: Some(0.5),
+        top_p: Some(1.0),
+        max_tokens: Some(50),
+        frequency_penalty: Some(0.0),
+        presence_penalty: Some(0.0),
+    };
+
+    let client = reqwest::Client::new();
+    let url = config.api_host + "/v1/chat/completions";
+    let api_key = config.openai_api_key;
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        format!("Bearer {}", api_key).parse().unwrap(),
+    );
+    headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+
+    let response = client
+        .post(url)
+        .headers(headers)
+        .json(&request)
+        .send()
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+
+    println!("{}", response);
+
+    Ok(())
 }
