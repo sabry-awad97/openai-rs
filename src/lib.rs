481bba6c12f4629f399b3b278505840dd4838bd6
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-11 16:29:07 +0300

    🎉 feat: Refactor to ChatClient struct and run method with error handling
    This commit adds a new `ChatClient` struct to handle chat requests to the specified API endpoint. The `run` method sends a chat completion request to the API and returns the response. It also handles any errors that may occur during the request and response process. The `log` crate is used to log the response and error messages.
    Overall, these changes seem to improve the code's readability, maintainability, and error handling.

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,51 @@
 mod chat;
-use chat::{
-    ChatCompletionMessage, ChatCompletionRequest, ChatCompletionResponse, MessageRole, OAIModel,
-};
-use log::{error, info};
-use reqwest::header::{HeaderValue, CONTENT_TYPE};
-
 use crate::chat::ChatError;
+use chat::ChatCompletionResponse;
+pub use chat::{ChatCompletionMessage, ChatCompletionRequest, MessageRole, OAIModel};
+use reqwest::header::{HeaderValue, CONTENT_TYPE};
+pub struct ChatClient {
+    api_host: String,
+    api_key: String,
+    client: reqwest::Client,
+}
 
-pub async fn run(api_host: &str, api_key: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let request = ChatCompletionRequest {
-        model: OAIModel::GPT3Turbo,
-        messages: vec![ChatCompletionMessage {
-            role: MessageRole::User,
-            content: "Hello, GPT-3!".to_string(),
-        }],
-        temperature: Some(0.5),
-        top_p: Some(1.0),
-        max_tokens: Some(50),
-        frequency_penalty: Some(0.0),
-        presence_penalty: Some(0.0),
-    };
-
-    let client = reqwest::Client::new();
-    let url = api_host.to_string() + "/v1/chat/completions";
-
-    let response = client
-        .post(url)
-        .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
-        .bearer_auth(api_key)
-        .json(&request)
-        .send()
-        .await?;
-
-    let status = response.status();
-    let response = response.json::<ChatCompletionResponse>().await?;
-
-    if status.is_success() {
-        info!("Response: {:?}", response);
-    } else {
-        error!("Error: {} - {:?}", status, response);
-        return Err(ChatError::ResponseError(status, response).into());
+impl ChatClient {
+    pub fn new(api_host: &str, api_key: &str) -> Self {
+        ChatClient {
+            api_host: api_host.to_string(),
+            api_key: api_key.to_string(),
+            client: reqwest::Client::new(),
+        }
     }
 
-    let result = response
-        .choices
-        .first()
-        .map(|choice| choice.message.content.clone())
-        .ok_or_else(|| ChatError::NoMessageReturned)?;
+    pub async fn run(&self, request: ChatCompletionRequest) -> Result<String, ChatError> {
+        let url = format!("{}/v1/chat/completions", self.api_host);
 
-    Ok(result)
+        let response = self
+            .client
+            .post(url)
+            .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
+            .bearer_auth(&self.api_key)
+            .json(&request)
+            .send()
+            .await?;
+
+        let status = response.status();
+        let response = response.json::<ChatCompletionResponse>().await?;
+
+        if status.is_success() {
+            log::info!("Response: {:?}", response);
+        } else {
+            log::error!("Error: {} - {:?}", status, response);
+            return Err(ChatError::ResponseError(status, response));
+        }
+
+        let result = response
+            .choices
+            .first()
+            .map(|choice| choice.message.content.clone())
+            .ok_or_else(|| ChatError::NoMessageReturned)?;
+
+        Ok(result)
+    }
 }
